91faab13108cdb69bcb7106ea895c386db64cb69
Vladimir Michael Eatwell <dimir@fb.com>
2021-06-16 15:31:58 +0100

    [watchos] fix arm64_32-apple-watchos spec

--- compiler/rustc_target/src/spec/arm64_32_apple_watchos.rs
+++ compiler/rustc_target/src/spec/arm64_32_apple_watchos.rs
@@ -7,9 +7,9 @@ pub fn target() -> Target {
         llvm_target: "arm64_32-apple-watchos".to_string(),
         pointer_width: 32,
         data_layout: "e-m:o-p:32:32-i64:64-i128:128-n32:64-S128".to_string(),
-        arch: "arm".to_string(),
+        arch: "aarch64".to_string(),
         options: TargetOptions {
-            features: "+strict-align".to_string(),
+            features: "+neon,+fp-armv8,+apple-a7".to_string(),
             max_atomic_width: Some(64),
             unsupported_abis: super::arm_base::unsupported_abis(),
             forces_embed_bitcode: true,
